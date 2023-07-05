8c5eec21fe939e879e5c7facbb53d04179e9c1da
Amineelm7 <elmnaoui.amine@gmail.com>
2023-07-05 10:19:39 +0100

    Initial commit

---
+++ src/main/java/com/example/gestion_marriage/GestionMarriageApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestion_marriage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionMarriageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionMarriageApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/example/gestion_marriage/GestionMarriageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestion_marriage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionMarriageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
